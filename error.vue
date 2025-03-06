6bbd625f52c4c62927e2ac566dc35d03e13948f0
inbarsoffcandex <inbar.soffer@candex.com>
2025-03-06 14:43:35 +0200

    Update error.vue

--- error.vue
+++ error.vue
@@ -1,10 +1,6 @@
-<script setup>
-import CandexLogo from './public/CandexLogo.png'
-</script>
-
 <template>
   <div class="flex flex-col items-center justify-center h-screen bg-white text-black text-2xl">
-    <img :src="CandexLogo" alt="CandexLogo" class="w-32 mb-4" />
+    <img src="/CandexLogo.png" alt="CandexLogo" class="w-32 mb-4" />
     <p>404 - Page Not Found</p>
   </div>
 </template>
