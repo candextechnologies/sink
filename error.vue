c469ae13df82aa26e90ace9f57694ba64d285313
inbarsoffcandex <inbar.soffer@candex.com>
2025-03-06 14:48:48 +0200

    Update error.vue

--- error.vue
+++ error.vue
@@ -1,6 +1,8 @@
+
 <template>
   <div class="flex flex-col items-center justify-center h-screen bg-white text-black text-2xl">
     <img src="public/CandexLogo.png" alt="CandexLogo" class="w-32 mb-4" />
     <p>404 - Page Not Found</p>
   </div>
 </template>
+
